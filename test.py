85e6a8de6e0522ab71d2046064572734b8d98d8e
Rashika Bhardwaj <rashikabhardwaj8159@gmail.com>
2025-01-09 10:53:59 +0530

    added

--- test.py
+++ test.py
@@ -1,1 +1,12 @@
-print('hello world')
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def student_intro():
+    name = "Rashika"
+    age = 20
+    course = "BCA"
+    return 
+ 
+
